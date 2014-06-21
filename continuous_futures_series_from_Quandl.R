4ed1a2ac9624bf37450731358f4398fc9ed31efc
Zhou Zhihao <cheerzzh@gmail.com>
2014-06-21 15:35:17 +0800

    reorganize

---
+++ continuous_futures_series_from_Quandl.R
@@ -0,0 +1,6 @@
+
+# how to obtain, stitch together, and clean data for backtesting using futures data from Quandl.
+# futures are not continuous in nature, and data for futures are harder to find.
+
+
+
