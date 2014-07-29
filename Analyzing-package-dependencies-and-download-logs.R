2fb2deac09632a590a720f55890740911191afa9
Jared Zhou <cheerzzh@gmail.com>
2014-07-29 17:50:10 +0800

    init r code for analyzing packages

---
+++ Analyzing-package-dependencies-and-download-logs.R
@@ -0,0 +1,12 @@
+
+# (1) relationships among available R packages in CRAN
+# (2) tracking the behavior of R users through download logs on CRAN mirrors
+# (3) building an R recommendation engine
+
+
+
+
+
+
+
+
